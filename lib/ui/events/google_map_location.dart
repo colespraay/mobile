9a550f83d4675d32883c35fddead0a628f5ca4ca
Daniel Esivue <daniel.esivue@cwg-plc.com>
2023-09-07 09:26:21 +0100

    Updated map

--- lib/ui/events/google_map_location.dart
+++ lib/ui/events/google_map_location.dart
@@ -25,10 +25,6 @@ class GoogleMapLocationScreen extends StatefulWidget {
 class _GoogleMapLocationScreenState extends State<GoogleMapLocationScreen> {
   final Completer<GoogleMapController> _controller = Completer<GoogleMapController>();
 
-  static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
 
 
 
@@ -53,7 +49,7 @@ class _GoogleMapLocationScreenState extends State<GoogleMapLocationScreen> {
 
   @override
   void initState() {
-    _setMarker( LatLng(37.42796133580664, -122.085749655962));
+    _setMarker( LatLng(widget.lat, widget.long));
   }
 
   @override
@@ -65,7 +61,7 @@ class _GoogleMapLocationScreenState extends State<GoogleMapLocationScreen> {
           GoogleMap(
             mapType: MapType.normal,
             markers: _markers.toSet(),
-            initialCameraPosition: _kGooglePlex,
+            initialCameraPosition: CameraPosition(target: LatLng(widget.lat, widget.long), zoom: 14.4746,),
             onMapCreated: (GoogleMapController controller) {
               _controller.complete(controller);
             },
@@ -85,65 +81,79 @@ class _GoogleMapLocationScreenState extends State<GoogleMapLocationScreen> {
 
 
   Widget buildContainer(){
-    return  ClipRRect(
-      borderRadius: BorderRadius.all(Radius.circular(24.r)),
-      child: Container(
-        margin: EdgeInsets.symmetric(vertical: 10.h, horizontal: 1.w),
-        decoration: BoxDecoration(color: Color(0xff1A1A21), borderRadius: BorderRadius.all(Radius.circular(18.r))),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding:  EdgeInsets.only(right:8.r, left: 10.w, top: 10.h, bottom: 10.h),
-              child: ClipRRect(
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(8.r), bottomLeft: Radius.circular(8.r)),
-                child: CachedNetworkImage(
-                  width: 110.w, height: 110.h,
-                  fit: BoxFit.cover,
-                  imageUrl:widget.event_CoverImage??"",
-                  placeholder: (context, url) => Center(child: SpinKitFadingCircle(size: 30,color: Colors.grey,)),
-                  errorWidget: (context, url, error) => Center(child: Icon(Icons.error)),
+    return  GestureDetector(
+      onTap:(){
+        _goToCurrentLocation();
+      },
+      child: ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(24.r)),
+        child: Container(
+          margin: EdgeInsets.symmetric(vertical: 10.h, horizontal: 1.w),
+          decoration: BoxDecoration(color: Color(0xff1A1A21), borderRadius: BorderRadius.all(Radius.circular(18.r))),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding:  EdgeInsets.only(right:8.r, left: 10.w, top: 10.h, bottom: 10.h),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(8.r), bottomLeft: Radius.circular(8.r)),
+                  child: CachedNetworkImage(
+                    width: 110.w, height: 110.h,
+                    fit: BoxFit.cover,
+                    imageUrl:widget.event_CoverImage??"",
+                    placeholder: (context, url) => Center(child: SpinKitFadingCircle(size: 30,color: Colors.grey,)),
+                    errorWidget: (context, url, error) => Center(child: Icon(Icons.error)),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(width: 6.w,),
-            Expanded(
-              child: Padding(
-                padding:  EdgeInsets.only(right: 16.w, top: 1.h, bottom: 0.h),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("${dayString(widget.event_date.toString())} ${monthString(widget.event_date.toString()??"")}, ${widget.eventTime}", style: CustomTextStyle.kTxtSemiBold.copyWith(fontSize: 16.sp, fontWeight: FontWeight.w500) ),
+              SizedBox(width: 6.w,),
+              Expanded(
+                child: Padding(
+                  padding:  EdgeInsets.only(right: 16.w, top: 1.h, bottom: 0.h),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("${dayString(widget.event_date.toString())} ${monthString(widget.event_date.toString()??"")}, ${widget.eventTime}", style: CustomTextStyle.kTxtSemiBold.copyWith(fontSize: 16.sp, fontWeight: FontWeight.w500) ),
 
-                    height10,
-                    Text(widget.eventname??"", style: CustomTextStyle.kTxtSemiBold.copyWith(fontSize: 16.sp,
-                        fontWeight: FontWeight.w500, color: CustomColors.sGreyScaleColor50),maxLines: 1, overflow: TextOverflow.ellipsis, ),
-                    height10,
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        SvgPicture.asset("images/location.svg"),
-                        SizedBox(width: 10.w,),
-                        SizedBox(
-                          width: 150.w,
-                          child: Text(widget.eventVenue??"", style: CustomTextStyle.kTxtSemiBold.copyWith(fontSize: 12.sp, fontWeight: FontWeight.w500, color: Color(0xffEEEEEE)),
-                            maxLines: 1, overflow: TextOverflow.ellipsis,),
-                        ),
-                      ],
-                    ),
+                      height10,
+                      Text(widget.eventname??"", style: CustomTextStyle.kTxtSemiBold.copyWith(fontSize: 16.sp,
+                          fontWeight: FontWeight.w500, color: CustomColors.sGreyScaleColor50),maxLines: 1, overflow: TextOverflow.ellipsis, ),
+                      height10,
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          SvgPicture.asset("images/location.svg"),
+                          SizedBox(width: 10.w,),
+                          SizedBox(
+                            width: 150.w,
+                            child: Text(widget.eventVenue??"", style: CustomTextStyle.kTxtSemiBold.copyWith(fontSize: 12.sp, fontWeight: FontWeight.w500, color: Color(0xffEEEEEE)),
+                              maxLines: 1, overflow: TextOverflow.ellipsis,),
+                          ),
+                        ],
+                      ),
 
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
 
 
-          ],
+            ],
 
+          ),
         ),
-      ),
 
+      ),
     );
+  }
+
+  Future<void> _goToCurrentLocation() async {
+    final GoogleMapController controller = await _controller.future;
+    await controller.animateCamera(CameraUpdate.newCameraPosition( CameraPosition(
+        bearing: 192.8334901395799,
+        target: LatLng(widget.lat, widget.long),
+        tilt: 59.440717697143555,
+        zoom: 19.151926040649414)));
   }
 }
